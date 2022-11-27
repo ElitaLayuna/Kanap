1ad1c56ab13be2a0ed2db53dcde44111de865022
Elita Layuna <elita1997@mail.ru>
2022-11-28 00:25:19 +0100

    Confirmation problem to fix / Thanks massage added

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,2 +1,18 @@
-const id = (new URL(document.location)).searchParams.get("id");
+//Function that creats a div where a thanks message will be displays
+function clientThanks() {
+    const confirmationP = document.querySelector(".confirmation p");
+    const newSpan = document.createElement("span");
+    newSpan.innerHTML = "<br> Nous vous remercions pour votre commande!";
+    newSpan.className = "customer's_thanks";
+    confirmationP.appendChild(newSpan);
+}
+
+clientThanks();
+
+//Function that displays order confirmation and order number
+function orderConfirmation() {
+    const id = (new URL(document.location)).searchParams.get("id");
 document.getElementById("orderId").innerHTML = id;
+}
+
+orderConfirmation();
